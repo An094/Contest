8821dc4f68349081ade47bedbef7edd937ab8eca
Ngo Thai An <an.ngothai@gameloft.com>
2023-10-17 16:27:32 +0700

    Add new asset and create the island

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,9 +10,12 @@ public class PlayerController : MonoBehaviour
 	private float m_MoveSpeed = 3f;
 	[SerializeField]
 	private Animator m_Animator;
+	[SerializeField] private Collider2D m_MoveUpDisableCollider;
+	[SerializeField] private LayerMask m_WhatIsGround;
+	[SerializeField] private Transform m_CeilingCheck;
 	private bool m_FacingRight = true;
-	
 
+	const float k_CeilingRadius = .2f;
 	private void Awake()
     {
 
@@ -36,18 +39,44 @@ public class PlayerController : MonoBehaviour
 
 	public void Move(Vector2 _move)
     {
+		//Debug.Log("Move.x =" + _move.x);
+
+		if (_move.y > 0)
+        {
+			if(m_MoveUpDisableCollider != null)
+            {
+				m_MoveUpDisableCollider.enabled = false;
+
+			}
+        }
+        else
+        {
+			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+			{
+				m_MoveUpDisableCollider.enabled = false;
+			}
+			else
+			{
+				if (m_MoveUpDisableCollider != null)
+				{
+					m_MoveUpDisableCollider.enabled = true;
+
+				}
+			}
+		}
+
 		m_Animator.SetFloat("Speed", _move.magnitude);
 		if(_move.x < 0 && m_FacingRight || _move.x > 0 && !m_FacingRight)
         {
 			Flip();
         }
-		Vector3 scaledMovement = m_MoveSpeed * Time.deltaTime * new Vector3(
+
+		Vector3 targetPosition = transform.position + new Vector3(
 			_move.x,
 			_move.y,
 			0
 		);
-
-		transform.Translate(scaledMovement);
+		transform.position = Vector3.Lerp(transform.position, targetPosition, m_MoveSpeed * Time.deltaTime);
 	}
 
 
